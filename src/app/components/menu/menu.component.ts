71fc3ede8bc9a7f8e59b21af1a5a25fc16231618
Alexaweb <alexaweb999@gmail.com>
2024-08-29 10:53:09 +0200

    anadimos function para abrir submenu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+// abrir submenu
+submenu = [false,false,false,false];
+
+submenuToggle (num: number) {
+  console.log (num);
+  for (let i =0; i<this.submenu.length; i++) {
+    if (i == num) {
+      this.submenu[i] = ! this.submenu[i];
+    } else {
+      this.submenu[i] = false
+    }
+  }
+}
 
 }
