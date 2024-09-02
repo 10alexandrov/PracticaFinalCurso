f9ed04ae48102c86b6924d324f3945134a2fee6f
Alexaweb <alexaweb999@gmail.com>
2024-09-02 16:45:41 +0200

    9.02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { ConteinerComponent } from './components/container/conteiner/conteiner.component'
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, ConteinerComponent],
+  imports: [RouterOutlet, MenuComponent, ConteinerComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
