29fd303fa92f4ffc08aaea2d98a7b6d679c765f1
Alexaweb <alexaweb999@gmail.com>
2024-09-04 22:48:07 +0200

    forms en processo validacion

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -1,26 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, AbstractControl, ValidatorFn } from '@angular/forms';
+
+
+export function matchPassword(c: AbstractControl): { [key: string]: boolean } | null {
+  const pass1 = c.get('u_password')?.value;
+  const pass2 = c.get('u_password2')?.value;
+  return pass1 === pass2 ? null : { match: true };
+ }
+
 
 @Component({
   selector: 'app-crear-usuario',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule],
-  templateUrl: './crear-usuario.component.html',
-  styleUrl: './crear-usuario.component.scss'
+  imports: [FormsModule,ReactiveFormsModule, CommonModule],
+  templateUrl:  './crear-usuario.component.html',
+  styleUrls: ['./crear-usuario.component.scss']
 })
 export class CrearUsuarioComponent implements OnInit{
 
-  public formCreateUser = new FormGroup ({
-    u_nombre: new FormControl (),
-    u_login: new FormControl (),
-    U_password: new FormControl (),
-    u_role: new FormControl ()
-  })
+  constructor(private fb: FormBuilder) {}
+  public formCreateUser!: FormGroup;
+
+  public sendDatos() {
+     if (this.formCreateUser.valid) {
+       console.log(this.formCreateUser.value);
+     } else {
+       console.log("Форма не валидна");
+       this.formCreateUser.markAllAsTouched(); // Помечаем все поля как затронутые, чтобы показать ошибки
+      }
+    }
 
   ngOnInit(): void {
-
-  }
+    this.formCreateUser = this.fb.group({
+       u_nombre: ['', Validators.required],
+       u_login: ['', Validators.required],
+       passGroup: this.fb.group({
+        u_password: ['', Validators.required],
+        u_password2: ['', Validators.required],
+       }, { validator: matchPassword }),
+        u_role: ['', Validators.required],
+        receiveInfo: new FormControl(true) });
+      }
 
 }
