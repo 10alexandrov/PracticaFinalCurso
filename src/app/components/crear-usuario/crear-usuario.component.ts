7ed212c2605c75ee860987ba24daf0694ece2502
Alexaweb <alexaweb999@gmail.com>
2024-09-06 13:47:16 +0200

    forms fin  validacion usuarios

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -2,13 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormControl, FormGroup, FormBuilder, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 
 export function matchPassword(c: AbstractControl): { [key: string]: boolean } | null {
   const pass1 = c.get('u_password')?.value;
   const pass2 = c.get('u_password2')?.value;
   return pass1 === pass2 ? null : { match: true };
+ }
+
+ export function passwordValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.value;
+  const hasNumber = /\d/.test(password);
+  const hasLetter = /[a-zA-Z]/.test(password);
+  const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const validLength = password.length >= 8;
+  if (!validLength || !hasNumber || !hasLetter || !hasSpecial) {
+     return { passwordStrength: true };
+     }
+   return null;
  }
 
 @Component({
@@ -36,11 +48,13 @@ export class CrearUsuarioComponent implements OnInit{
       this.formCreateUser = this.fb.group({
         u_nombre: ['', Validators.required],
         u_login: ['', Validators.required],
+
         passGroup: this.fb.group({
-          u_password: ['', Validators.required],
+          u_password: ['', [Validators.required, Validators.minLength(8), passwordValidator]], // Проверка длины и силы пароля
           u_password2: ['', Validators.required],
-        }, { validator: matchPassword }),
+         }, { validator: matchPassword }),
         u_role: ['', Validators.required],
+        u_active: [true],                        // User active default = true
         receiveInfo: [true]
       });
     }
